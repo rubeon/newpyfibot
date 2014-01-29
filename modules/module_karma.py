6b0925bafcab88e522c5c9c51427ccd9e91b113b
Eric Williams <eric7292@ewilliam-mb.eur.rackspace.corp>
2014-01-29 14:23:06 +0000

    Added ability to put multiple karmas in one message. Added throttle to keep people from karma-flooding.

--- modules/module_karma.py
+++ modules/module_karma.py
@@ -3,16 +3,69 @@
 # TODO:
 # - Throttling
 # - unfucking the karma message parser (_parse_karma, do_karma)
-
+# - disallowing users to affect their own karma
 
 import re
 import sqlite3
+import md5 # used for check_delay
+import time # used for check_delay
+import random # used to choose random item from 'stops'
+
+dudes = [
+    "man",
+    "dude",
+    "buddy",
+    "pal",
+    "muchacho",
+    "caballero",
+    "cowboy",
+    "sonny",
+    "cabron"
+]
+
+stops = [
+    "Ow",
+    "Quit it",
+    "Stahp",
+    "Stop it",
+    "Go away",
+    "Beat it",
+    "That's enough, now",
+    "Nope",
+    "That's not going to happen",
+    "One more time and I'm out of here",
+    "Final warning, no way",
+]
+karma_delay = 600
+
+# karma_actions is used to check delays for now.
+# it's a dictionary of hashes mapped to time.time() values
+karma_actions = {}
+
+
+def check_delay(bot, user, channel, karma_hash):
+    """
+    checks that <user> hasn't tried to change the karma for <item> in at least <delay>.
+    Uses an in-memory hash to check the date
+    """
+    print "check_delay", karma_hash
+    # karma_hash = md5.new(user + item).hexdigest()
+    timestamp = time.time()
+    # check the time of the previous karma adjustment, if any
+    previous_action = karma_actions.get(karma_hash,0)
+    
+    if timestamp - previous_action > karma_delay:
+        karma_actions[karma_hash] = timestamp
+        print karma_actions
+        return True
+    else:
+        return False
 
 def do_karma(bot, user, channel, karma):
     """
     perform karma adjustment
     """
-    print "do_karma " + 20 * "-" + karma
+    print "::do_karma " + 20 * "-" + "|".join(karma)
     if "++" in karma[0] or "--" in karma[0]:
         # this is the actual karma command. Hack?
         return
@@ -24,6 +77,15 @@ def do_karma(bot, user, channel, karma):
     conn = sqlite3.connect('karma.db')
     c = conn.cursor()
     t = (karma[0].lower(),)
+    
+    # check the delay
+    print "::t=", t[0]
+    karma_hash = md5.new(user + t[0]).hexdigest()
+    timestamp = time.time()
+
+    if not check_delay(bot, user, channel, karma_hash):
+        return bot.say(channel, "%s, %s" % (random.choice(stops), random.choice(dudes)))
+        
     c.execute('select * from karma where word=?', t)
     res = c.fetchone()
 
@@ -50,19 +112,51 @@ def do_karma(bot, user, channel, karma):
 
 def handle_privmsg(bot, user, reply, msg):
     """Grab karma changes from the messages and handle them"""
-
+    print "::handle_privmsg"
     # m = re.findall('((?u)[\w.`\']+)(\+\+|\-\-)', msg.decode('utf-8'))
     # if len(m) == 0 or len(m) >= 5: return None
-
-    # for k in m:
-    #     do_karma(bot, user, reply, k)
+    #
+    #for k in m:
+    #    print "::", k
+    #    do_karma(bot, user, reply, k)
     
-    _parse_karma(msg)
+    """
+    This method is used to parse karma from a message.
+    It should allow for the following karma syntax:
+    thing--
+    thing++    
+    thing++
+    ++thing
+    thing1++ thing2++ thing3-- thing4--
+    ++thing1 ++thing2 --thing3 --thing4
+    thing++ is much better than thing--
+    """
+    
+    # parse the message into tokens
+    tokens = msg.split()
+    # walk through the tokens
 
-    return
+    for token in tokens:
+        # is it a ++ or a --?
+        if token.find("++")!=-1:
+            # there's karma thar
+            print "::Adding +1 to %s" % token
+            k = token.replace("++","")
+            action = "++"
+        elif token.find("--")!=-1:
+            print "::Subtracting 1 from %s" % token
+            k = token.replace("--","")
+            action = "--"
+        else:
+            print "::Ignoring %s" % token
+            continue
+
+        do_karma(bot, user, reply, (k, action))
+        
 
 def handle_action(bot, user, reply, msg):
     """Grab karma changes from the messages and handle them"""
+    print "handle_action"
 
     m = re.findall('((?u)[\w.`\']+)(\+\+|\-\-)', msg.decode('utf-8'))    
     if len(m) == 0 or len(m) >= 5: return None
@@ -76,7 +170,7 @@ def command_karma(bot, user, channel, args):
     """.karma <item>"""
     # return bot.say(channel, "|".join(args))
     print "command_karma: ARGS:", args
-    # item = args.split()[0]
+    item = args.split()[0]
     if item.find("++")!=-1 or item.find("--")!=-1:
         return
     
@@ -139,34 +233,6 @@ def command_topkarma(bot, user, channel, args):
     return
 
 
-def _parse_karma(msg):
-    """
-    This method is used to parse karma from a message.
-    It should allow for the following karma syntax:
-    thing--
-    thing++    
-    thing++
-    ++thing
-    thing1++ thing2++ thing3-- thing4--
-    ++thing1 ++thing2 --thing3 --thing4
-    thing++ is much better than thing--
-    """
-    
-    # parse the message into tokens
-    tokens = msg.split()
-    # walk through the tokens
-
-    for token in tokens:
-        # is it a ++ or a --?
-        if token.find("++")!=-1:
-            print "Adding +1 to %s" % token
-        elif token.find("--")!=-1:
-            print "Subtracting 1 from %s" % token
-
-        else:
-            print "Ignoring %s" % token
-    
-    # process the ++ or --
 
 
 if __name__ == "__main__":
